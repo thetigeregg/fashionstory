1243f49e35827d7fa20daffb892ccb2a30b4b10b
thetigeregg <thetigeregg@gmail.com>
2022-08-14 23:09:12 -0400

    Populate hairstyle selector with JSON data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { FormControl, FormGroup } from "@angular/forms";
+import { Item } from "src/models/item";
+import HairstylesJson from "../../src/data/hairstyles.json";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  hairstyles: Item[] = HairstylesJson;
   title = "fashionstory";
+  loading = true;
 
   characterSexes: string[] = ["Male", "Female"];
 
   optionForm = new FormGroup({
-    selectedSex: new FormControl("")
+    selectedSex: new FormControl(""),
+    selectedHairstyles: new FormControl("")
   });
+
+  ngOnInit(): void {}
 }
