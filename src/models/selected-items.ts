63047c64d46c86c18ad06080d304ed3757631e27
thetigeregg <thetigeregg@gmail.com>
2022-08-14 23:36:12 -0400

    Fix Body import in SelectItems interface
    Made all members optional in SelectedItems interface

--- src/models/selected-items.ts
+++ src/models/selected-items.ts
@@ -1,18 +1,17 @@
 import { Item } from "./item";
-
+import { Body } from "./body";
 export interface SelectedItems {
-  Body: Body;
-  Head: Body;
-  Hair: Item;
-  Face: Item;
-  "Face Accessory": Item;
-  "Eye Decoration": Item;
-  Earrings: Item;
-  Cape: Item;
-  Hat: Item;
-  Overall: Item;
-  Glove: Item;
-  Shield: Item;
-  Shoes: Item;
-  "Shining Rod": Item;
+  Body?: Body;
+  Head?: Body;
+  Hair?: Item;
+  Face?: Item;
+  "Face Accessory"?: Item;
+  "Eye Decoration"?: Item;
+  Earrings?: Item;
+  Cape?: Item;
+  Hat?: Item;
+  Overall?: Item;
+  Glove?: Item;
+  Shield?: Item;
+  Shoes?: Item;
 }
