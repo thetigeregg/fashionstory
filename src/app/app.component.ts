077ebbd2efd08c9b2bc37639214e338219494019
thetigeregg <thetigeregg@gmail.com>
2022-08-14 22:03:38 -0400

    Add basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 
+import { FormControl, FormGroup } from "@angular/forms";
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -7,4 +9,10 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "fashionstory";
+
+  characterSexes: string[] = ["Male", "Female"];
+
+  optionForm = new FormGroup({
+    selectedSex: new FormControl("")
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
+import { MatSelectModule } from "@angular/material/select";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
